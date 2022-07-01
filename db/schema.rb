7a29fbcfb7e8cdb967690886954321f8699a2c53
daiki_yamada <aristrat@icloud.com>
2022-07-02 00:07:36 +0900

    Chapter4-3-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_091215) do
+ActiveRecord::Schema.define(version: 2022_07_01_092654) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
