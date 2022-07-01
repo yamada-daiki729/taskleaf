84f7473ad3da242e530cea8ec9a6adf22209c23a
daiki_yamada <aristrat@icloud.com>
2022-07-01 11:36:15 +0900

    Taskleaf finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,25 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice:"タスク「#{task.name}」を更新しました"
   end
 
   def create
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url,notice: "タスク「#{task.name}」を登録しました"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url,notice: "タスクを「#{task.name}」削除しました"
   end
 
   private
